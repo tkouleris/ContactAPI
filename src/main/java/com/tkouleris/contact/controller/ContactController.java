e2f2c7d04320c9630eea1643bec7a811d0c07d19
darkheart101 <kurokokoro@windowslive.com>
2020-01-13 06:46:16 +0200

    fix

--- src/main/java/com/tkouleris/contact/controller/ContactController.java
+++ src/main/java/com/tkouleris/contact/controller/ContactController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tkouleris.contact.Model.Contact;
@@ -16,6 +17,7 @@ public class ContactController {
 	ContactRepository R_Contact;
 	
 	@GetMapping(value="/all")
+	@ResponseBody
 	public List<Contact> getAll(){		
 		return (List<Contact>) R_Contact.findAll();		
 	}
