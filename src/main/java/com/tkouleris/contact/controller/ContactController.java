0911260b6d88880906d42eca3dac094905c76480
darkheart101 <kurokokoro@windowslive.com>
2020-01-15 06:12:35 +0200

    feature: get one contact

--- src/main/java/com/tkouleris/contact/controller/ContactController.java
+++ src/main/java/com/tkouleris/contact/controller/ContactController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,12 @@ public class ContactController {
 	@ResponseBody
 	public List<Contact> getAll(){		
 		return (List<Contact>) R_Contact.findAll();		
+	}
+	
+	@GetMapping(value="contacts/{contact_id}")
+	public Contact getContact(@PathVariable("contact_id") long contact_id)
+	{		
+		return R_Contact.findById(contact_id).orElse(null);
 	}
 	
 	@PostMapping(path = "contacts/create", consumes = "application/json", produces = "application/json")
