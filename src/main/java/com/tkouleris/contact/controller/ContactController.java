7ab6d4f781e2a4bb11c50f1b8c3d83bd56f0601a
darkheart101 <kurokokoro@windowslive.com>
2020-01-15 05:59:58 +0200

    feature: create new contact endpoint

--- src/main/java/com/tkouleris/contact/controller/ContactController.java
+++ src/main/java/com/tkouleris/contact/controller/ContactController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +18,17 @@ public class ContactController {
 	@Autowired
 	ContactRepository R_Contact;
 	
-	@GetMapping(value="/all")
+	@GetMapping(value="contacts/all")
 	@ResponseBody
 	public List<Contact> getAll(){		
 		return (List<Contact>) R_Contact.findAll();		
 	}
 	
+	@PostMapping(path = "contacts/create", consumes = "application/json", produces = "application/json")
+	@ResponseBody
+	public Contact create(@RequestBody Contact contact)
+	{	
+		return R_Contact.save(contact);
+	}
 
 }
