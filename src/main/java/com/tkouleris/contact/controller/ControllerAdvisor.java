943c4e4f7b3b408585aad53f85f8d1dd2468ef9b
darkheart101 <kurokokoro@windowslive.com>
2020-01-21 21:13:25 +0200

    fix: Controller Advice fix

--- src/main/java/com/tkouleris/contact/controller/ControllerAdvisor.java
+++ src/main/java/com/tkouleris/contact/controller/ControllerAdvisor.java
@@ -3,7 +3,6 @@ package com.tkouleris.contact.controller;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
