1ed538b88e7e5f92fc23e0c823e3e9213273ae9a
darkheart101 <kurokokoro@windowslive.com>
2020-02-25 21:22:35 +0200

    feature: swagger api information

--- src/main/java/com/tkouleris/contact/ContactsApiApplication.java
+++ src/main/java/com/tkouleris/contact/ContactsApiApplication.java
@@ -1,8 +1,16 @@
 package com.tkouleris.contact;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,6 +19,29 @@ public class ContactsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApiApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfiguration()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/contacts/*"))
+				.build()
+				.apiInfo(apiDetails());
+		
+	}
+	
+	private ApiInfo apiDetails()
+	{
+		return new ApiInfo("Contacts API"
+			, "The sole purpose of this api is to start learning Spring Boot by developing an API that holds and "
+			+ "retrives contacts."
+			, "1.0"
+			, "Free to use"
+			, new springfox.documentation.service.Contact("Thodoris Kouleris", "http://tkouleris.eu/", "tkouleris@gmail.com")
+			, "http://tkouleris.eu/"
+			,null, Collections.emptyList());
 	}
 
 }
