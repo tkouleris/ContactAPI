185c2ff9a63e0df78aa70739f41ed70824035e10
darkheart101 <kurokokoro@windowslive.com>
2021-03-01 06:07:07 +0200

    restart project

--- src/main/java/com/tkouleris/contact/dao/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tkouleris.contact.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tkouleris.contact.Model.Contact;
-
-public interface ContactRepository extends CrudRepository<Contact, Long>{
-
-}
